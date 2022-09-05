faa0efdf6e58b9333f393ecc2b08087032fd7558
Joseph K Justus <justuskasyoki57@gmail.com>
2022-09-05 16:40:03 +0300

    Add project files.

---
+++ Exercise2(a)/LinkedListHelpers.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Exercise2_a_
+{
+    public class LinkedListHelpers
+    {
+      public class node
+        {
+            public string val;
+            public node next;
+            public node(string val) { this.val = val; }
+        }
+     public  static void removeDuplicate(node head)
+        {
+            HashSet<string> hs = new HashSet<string>();
+
+            node current = head;
+            node prev = null;
+            while (current != null)
+            {
+                string curval = current.val;
+                if (hs.Contains(curval))
+                {
+                    prev.next = current.next;
+                }
+                else
+                {
+                    hs.Add(curval);
+                    prev = current;
+                }
+                current = current.next;
+            }
+        }
+        // Function to print nodes in a
+        // given linked list
+       public static void printList(node head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.val + " ");
+                head = head.next;
+            }
+        }
+
+    }
+
+}
